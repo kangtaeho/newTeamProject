79d7cf2618e2b74e90f58ee30d2a16ce0a0db513
wkrqud12 <34785957+wkrqud12@users.noreply.github.com>
2017-12-26 20:06:27 +0900

    병철이이이이

---
+++ teamProject/stage1.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "gameNode.h"
+class stage1 : public gameNode // 
+{
+public:
+	stage1();
+	~stage1();
+};
+
---
+++ teamProject/stage2.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "gameNode.h"
+class stage2 : public gameNode
+{
+public:
+	stage2();
+	~stage2();
+};
+
