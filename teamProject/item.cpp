a929a866c870b39b820d7671decc532bbb8f6174
preun <woghtjdah@naver.com>
2017-12-27 19:18:31 +0900

    아이템 v1.0

--- teamProject/item.cpp
+++ teamProject/item.cpp
@@ -9,4 +9,56 @@ item::item()
 
 item::~item()
 {
+
+}
+
+HRESULT item::init()
+{
+
+	return S_OK;
+}
+
+//오브젝트(투척)용		이미지 키,    이미지 파일이름,   넓이,     높이,   좌표			데미지 
+HRESULT item::init(string strKey, const char* fileName, int width, int height, POINT point, int itemEffect)
+{
+	_itemImage = IMAGEMANAGER->addImage(strKey, fileName, width, height, true, RGB(255, 0, 255));
+	_x = point.x;
+	_y = point.y;
+	_itemType = THROW;
+	_itemEffect = itemEffect;
+	_price = 0;
+
+
+	return S_OK;
+}
+/*
+image* _itemImage;		//아이템 이미지
+float _x, _y;			//아이템 좌표
+tagItemType	_itemType;	//아이템 종류
+int _itemEffect;		//아이템 효과(투척 -> 데미지 & 물약 -> 회복)
+int _price;				//아이템 가격
+*/
+//상점(물약)용		이미지 키,    이미지 파일이름,   넓이,     높이,           좌표		회복량			가격  *** 프레임이 아닌 그냥 이미지로 만들어둠!!!!
+HRESULT item::init(string strKey, const char* fileName, int width, int height, POINT point, int itemEffect, int price)
+{
+	_itemImage = IMAGEMANAGER->addImage(strKey, fileName, width, height, true, RGB(255, 0, 255));
+	_x = point.x;
+	_y = point.y;
+	_itemType = POTION;
+	_itemEffect = itemEffect;
+	_price = price;
+
+	return S_OK;
+}
+void item::release()
+{
+
+}
+void item::update()
+{
+
+}
+void item::render()
+{
+
 }
--- teamProject/item.h
+++ teamProject/item.h
@@ -1,10 +1,46 @@
 #pragma once
 #include "gameNode.h"
+
+enum tagItemType{
+	POTION,		//물약 소모품
+	THROW		//투척 소모품(맵 object)
+};
 class item :
 	public gameNode
 {
+private:
+	image* _itemImage;		//아이템 이미지
+	float _x, _y;			//아이템 좌표
+	tagItemType	_itemType;	//아이템 종류
+	int _itemEffect;		//아이템 효과(투척 -> 데미지 & 물약 -> 회복)
+	int _price;				//아이템 가격
+
+
 public:
 	item();
 	~item();
+
+	
+	HRESULT init();
+
+	
+	//오브젝트(투척)용		이미지 키,    이미지 파일이름,   넓이,     높이,   좌표			데미지  *** 프레임이 아닌 그냥 이미지로 만들어둠!!!!
+	HRESULT init(string strKey, const char* fileName, int width, int height, POINT point, int itemEffect);
+
+	//상점(물약)용		이미지 키,    이미지 파일이름,   넓이,     높이,           좌표		회복량			가격  *** 프레임이 아닌 그냥 이미지로 만들어둠!!!!
+	HRESULT init(string strKey, const char* fileName, int width, int height, POINT point, int itemEffect, int price);
+
+	void release();
+	void update();
+	void render();
+
+	//아이템 좌표 설정 (투척시 사용)
+	void setPoint(POINT point){ _x = point.x; _y = point.y; }	
+	
+	//아이템 효과 가져오기(투척아이템 : 데미지, 물약 : 회복량)
+	int getItemEffect(){
+		return _itemEffect;
+	}
+
 };
 
