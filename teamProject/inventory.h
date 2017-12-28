58863601d708074e84be8f483d6214112fc73ff4
kangkangtaeho <whitevines@naver.com>
2017-12-28 11:01:10 +0900

    인벤토리

--- teamProject/inventory.h
+++ teamProject/inventory.h
@@ -1,8 +1,23 @@
 #pragma once
-class inventory
+#include "gameNode.h"
+#include "orangePotion.h"
+#include "redPotion.h"
+#include "whitePotion.h"
+
+class inventory:public gameNode
 {
+private:
+
 public:
 	inventory();
 	~inventory();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	void getItem();						//æ∆¿Ã≈∆¿ª ¡÷ø¸¿ª ∂ß
+
 };
 
