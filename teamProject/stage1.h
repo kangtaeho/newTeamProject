cfaf8ea74953bbb502cb5aa5d3b549999585d11f
preun <woghtjdah@naver.com>
2017-12-28 14:32:00 +0900

    아이템클래스 함수추가

--- teamProject/stage1.h
+++ teamProject/stage1.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "gameNode.h"
+#include "item.h"
+#include "stone.h"
 class stage1 : public gameNode
 {
 
 private:
 	image* _stage1;
 
+	item* _stone;
 public:
 	stage1();
 	~stage1();
