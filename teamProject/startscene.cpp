ee5f26d03b7d904fb65929b22bd63588fded9172
preun <woghtjdah@naver.com>
2017-12-29 15:12:26 +0900

    스타트씬 화살표이동

--- teamProject/startscene.cpp
+++ teamProject/startscene.cpp
@@ -16,7 +16,7 @@ HRESULT startscene::init()
 	IMAGEMANAGER->addImage("selectScene", "./images/background.bmp", 1152, 648, true, RGB(255, 0, 255));
 	IMAGEMANAGER->addImage("selecttri", "./images/selecttri.bmp", 30, 35, true, RGB(255, 0, 255));
 
-
+	_index = true;
 	return S_OK;
 }
 void startscene::release()
@@ -27,11 +27,33 @@ void startscene::update()
 {
 	if (KEYMANAGER->isOnceKeyDown('X'))
 	{
+		if (_index)
 		SCENEMANAGER->changeScene("스테이지00", false);
+
+		//옵션화면 미구현
+		/*else
+			SCENEMANAGER->changeScene("스테이지00", false);*/
+	}
+	if (KEYMANAGER->isOnceKeyDown(VK_DOWN))
+	{
+		if (_index)
+			_index = false;
+		else
+			_index = true;
+	}
+	else if (KEYMANAGER->isOnceKeyDown(VK_UP))
+	{
+		if (!_index)
+			_index = true;
+		else
+			_index = false;
 	}
 }
 void startscene::render()
 {
 	IMAGEMANAGER->findImage("selectScene")->render(getMemDC(),0, 0);
+	if (_index)
 	IMAGEMANAGER->findImage("selecttri")->render(getMemDC(), 417, 375);
+	else
+		IMAGEMANAGER->findImage("selecttri")->render(getMemDC(), 417, 445);
 }
--- teamProject/startscene.h
+++ teamProject/startscene.h
@@ -3,6 +3,7 @@
 class startscene :
 	public gameNode
 {
+	bool _index;
 public:
 	startscene();
 	~startscene();
