d0d94e0b90f01785a953237fc00e16882a7469e4
kangkangtaeho <whitevines@naver.com>
2017-12-29 15:15:14 +0900

    인벤 약간 수정

--- teamProject/inventory.cpp
+++ teamProject/inventory.cpp
@@ -17,7 +17,10 @@ HRESULT inventory::init() {
 
 	_blackWindowBmp = IMAGEMANAGER->addImage("흑백창", "./images/backWindow.bmp", 1152, 648, true, RGB(255, 0, 255));
 	_inventoryBmp = IMAGEMANAGER->addImage("인벤토리창", "./images/inventory.bmp", 280, 396, true, RGB(255, 0, 255));
+	_shopBmp = IMAGEMANAGER->addImage("상점창", "./images/shop.bmp", 560, 396, true, RGB(255, 0, 255));
 
+	_RPotion = new redPotion;
+	_RPotion->init();
 	
 	return S_OK;
 }
@@ -28,18 +31,20 @@ void inventory::release() {
 
 void inventory::update() {
 
+	_RPotion->update();
 }
 
 void inventory::render() {
 	
 	_blackWindowBmp->alphaRender(getMemDC(), 100);
-	_inventoryBmp->render(getMemDC(), WINSIZEX / 2 - _inventoryBmp->getWidth() / 2, WINSIZEY / 2 - _inventoryBmp->getHeight() / 2);
+	_shopBmp->render(getMemDC(), WINSIZEX / 2 - _shopBmp->getWidth() / 2, WINSIZEY / 2 - _shopBmp->getHeight() / 2);
+	//_inventoryBmp->render(getMemDC(), WINSIZEX / 2 - _inventoryBmp->getWidth() / 2, WINSIZEY / 2 - _inventoryBmp->getHeight() / 2);
 	
 }
 
 void inventory::shopState() {
 
-	if (!_openShop) return;
+	if (!_openShop) return;	
 
 }
 
--- teamProject/inventory.h
+++ teamProject/inventory.h
@@ -10,11 +10,23 @@ private:
 	image* _blackWindowBmp;				// 흑백화면 만들기 위한 검정색 윈도우 창
 	image* _inventoryBmp;				// 인벤토리 이미지
 	image* _shopBmp;					// 상점 이미지
+	image* _selectBoxBmp;				// 선택 박스
 
-	int _currentMoney;
+	image* _redPotionNumberBmp;			// 빨간포션 숫자 이미지
+	image* _orangePotionNumberBmp;		// 주황포션 숫자 이미지
+	image* _whitePotionNumberBmp;		// 하얀색포션 숫자 이미지
+
+	POINT _selectShopPos;				// 상점에서 선택박스 움직일 때 좌표
+	POINT _selectInvenPos;				// 인벤토리에서 선택박스 움직일 때 좌표
+	
+	int _currentMoney;					// 현재 가진 돈
 
 	bool _openShop;
 	bool _openInventory;
+
+	redPotion* _RPotion;
+	orangePotion* _OPotion;
+	whitePotion* _WPotion;
 
 public:
 	inventory();
@@ -28,7 +40,7 @@ public:
 	void shopState();					//상점
 	void inventoryState();				//내 인벤토리
 
-	void openShop() { _openShop = true; }						// 상점열기
+	void openShop() { _openShop = true; }							// 상점열기
 	void openInventory() { _openInventory = true; }					// 인벤토리열기
 	void closeShop() { _openShop = false; }
 	void closeInventory() { _openInventory = false; }
