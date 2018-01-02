dc0cf65f4bcbf93ed95ba9c392b4fe97954cf3ac
preun <woghtjdah@naver.com>
2018-01-02 18:40:09 +0900

    .

--- teamProject/stage3.cpp
+++ teamProject/stage3.cpp
@@ -3,6 +3,11 @@
 
 
 stage3::stage3()
+:_rc1(RectMakeCenter(50, 300, 100, 100)),	//절대값같은 수치는 여기서 초기화, 이미지의 크기처럼 무언가를 받아서 초기화 해야한다면 init에서
+_currentRC(&_rc1),
+_firstWave(false),
+_secondWave(false),
+_thirdWave(false)
 {
 }
 
@@ -13,25 +18,16 @@ stage3::~stage3()
 
 HRESULT stage3::init()
 {
-	IMAGEMANAGER->addImage("스테이지_02", "./images/03_stage00.bmp", 5809, 1251, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addImage("스테이지_02_red", "./images/03_stage00_red.bmp", 5809, 1251, true, RGB(255, 0, 255));
+	addImage();			//이미지 add
+	initialization();	//멤버변수 초기화(ex. 아이템 등)
+	singletonInit();	//싱글톤 초기화(ex. 카메라 or 스테이지 사운드)
 
-
-	//카메라 렉트
-	rc1 = RectMakeCenter(50, 300, 100, 100);
-	currentRC = &rc1;
-
-	_inven = new inventory;
-	_inven->init();
-
-	//
-	CAMERAMANAGER->setCameraCondition(CAMERA_AIMING);
-	CAMERAMANAGER->setCameraAim(&rc1);
 	return S_OK;
 }
 
 void stage3::release()
 {
+
 }
 
 void stage3::update()
@@ -44,13 +40,13 @@ void stage3::update()
 				if (CAMERAMANAGER->getCameraCondition() == CAMERA_FREE)
 				{
 					CAMERAMANAGER->cameraMove(false, 0);
-					currentRC->bottom -= 10;
-					currentRC->top -= 10;
+					_currentRC->bottom -= 10;
+					_currentRC->top -= 10;
 				}
 				else
 				{
-					currentRC->bottom -= 10;
-					currentRC->top -= 10;
+					_currentRC->bottom -= 10;
+					_currentRC->top -= 10;
 				}
 			}
 		}
@@ -62,12 +58,12 @@ void stage3::update()
 				if (CAMERAMANAGER->getCameraCondition() == CAMERA_FREE)
 				{
 					CAMERAMANAGER->cameraMove(false, 0);
-					currentRC->bottom += 10;
-					currentRC->top += 10;
+					_currentRC->bottom += 10;
+					_currentRC->top += 10;
 				}
 				else {
-					currentRC->bottom += 10;
-					currentRC->top += 10;
+					_currentRC->bottom += 10;
+					_currentRC->top += 10;
 				}
 			}
 		}
@@ -79,18 +75,18 @@ void stage3::update()
 				if (CAMERAMANAGER->getCameraCondition() == CAMERA_FREE)
 				{
 					CAMERAMANAGER->cameraMove(true, 0);
-					currentRC->left -= 10;
-					currentRC->right -= 10;
+					_currentRC->left -= 10;
+					_currentRC->right -= 10;
 				}
 				else
 				{
-					if (currentRC->left <= 0)
+					if (_currentRC->left <= 0)
 					{
-						currentRC->left += 10;
-						currentRC->right += 10;
+						_currentRC->left += 10;
+						_currentRC->right += 10;
 					}
-					currentRC->left -= 10;
-					currentRC->right -= 10;
+					_currentRC->left -= 10;
+					_currentRC->right -= 10;
 				}
 			}
 		}
@@ -102,26 +98,26 @@ void stage3::update()
 				if (CAMERAMANAGER->getCameraCondition() == CAMERA_FREE)
 				{
 					CAMERAMANAGER->cameraMove(true, 0);
-					currentRC->left += 10;
-					currentRC->right += 10;
+					_currentRC->left += 10;
+					_currentRC->right += 10;
 				}
 				else
 				{
-					if (currentRC->right >= 3456)
+					if (_currentRC->right >= 3456)
 					{
-						currentRC->left -= 10;
-						currentRC->right -= 10;
+						_currentRC->left -= 10;
+						_currentRC->right -= 10;
 					}
-					currentRC->left += 10;
-					currentRC->right += 10;
+					_currentRC->left += 10;
+					_currentRC->right += 10;
 				}
 			}
 		}
 
 		if (KEYMANAGER->isOnceKeyDown('W'))
 		{
-			currentRC = &rc1;
-			CAMERAMANAGER->setCameraAim(currentRC);
+			_currentRC = &_rc1;
+			CAMERAMANAGER->setCameraAim(_currentRC);
 			CAMERAMANAGER->setCameraCondition(CAMERA_AIMING);
 
 		}
@@ -144,12 +140,47 @@ void stage3::update()
 
 void stage3::render()
 {
-	//이걸로 카메라처리 해주센
-	IMAGEMANAGER->findImage("스테이지_02")->render(getMemDC(), 0, 0, CAMERAMANAGER->getCameraPoint().x, CAMERAMANAGER->getCameraPoint().y, WINSIZEX, WINSIZEY);
-	IMAGEMANAGER->findImage("스테이지_02_red")->render(getMemDC(), 0, 0, CAMERAMANAGER->getCameraPoint().x, CAMERAMANAGER->getCameraPoint().y, WINSIZEX, WINSIZEY);
-	RectangleMake(getMemDC(), CAMERAMANAGER->CameraRelativePoint(rc1).x, CAMERAMANAGER->CameraRelativePoint(rc1).y, 100, 100);
-	_inven->render();
+	draw();
 	//이거 지우고
 //IMAGEMANAGER->findImage("스테이지_02")->render(getMemDC(), 0, 0);
 //IMAGEMANAGER->findImage("스테이지_02_red")->render(getMemDC(), 0, 0);
+}
+void stage3::dropMoney(POINT point, int won)	//돈 드랍 함수 몬스터가 죽었을경우 이것 호출하면 됩니다.
+{
+
+}
+
+void stage3::makeEnemy()	//몬스터 생성 함수
+{
+
+}
+
+void stage3::characterMovement()							//캐릭터 키매지저를 관리하는 함수
+{
+
+}
+
+void stage3::addImage()		//이미기 추가해주는 함수 이후 이미지는 여기서 add하는걸로
+{
+	IMAGEMANAGER->addImage("스테이지_02", "./images/03_stage00.bmp", 5809, 1251, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("스테이지_02_red", "./images/03_stage00_red.bmp", 5809, 1251, true, RGB(255, 0, 255));
+
+}
+void stage3::initialization()	//변수들 new선언 및 init 해주는 함수 이후 new 및 init은 여기서 하는걸로
+{
+	_inven = new inventory;
+	_inven->init();
+}
+void stage3::singletonInit()	//init에서 싱글톤들 세팅해주는 함수 이후 세팅은 여기서 하는걸로		  
+{
+	CAMERAMANAGER->setCameraCondition(CAMERA_AIMING);
+	CAMERAMANAGER->setCameraAim(&_rc1);
+}
+void stage3::draw()			//그려주는 함수 이후 렌더는 여기서 하는걸로								
+{
+	//이걸로 카메라처리 해주센
+	IMAGEMANAGER->findImage("스테이지_02")->render(getMemDC(), 0, 0, CAMERAMANAGER->getCameraPoint().x, CAMERAMANAGER->getCameraPoint().y, WINSIZEX, WINSIZEY);
+	IMAGEMANAGER->findImage("스테이지_02_red")->render(getMemDC(), 0, 0, CAMERAMANAGER->getCameraPoint().x, CAMERAMANAGER->getCameraPoint().y, WINSIZEX, WINSIZEY);
+	RectangleMake(getMemDC(), CAMERAMANAGER->CameraRelativePoint(_rc1).x, CAMERAMANAGER->CameraRelativePoint(_rc1).y, 100, 100);
+	_inven->render();
 }
--- teamProject/stage3.h
+++ teamProject/stage3.h
@@ -2,6 +2,7 @@
 #include "gameNode.h"
 #include "character.h"
 #include "inventory.h"
+#include "money.h"
 /*
 static enum stageState
 {
@@ -16,10 +17,15 @@ class stage3 : public gameNode
 {
 private:
 
-	RECT rc1;
-	RECT* currentRC;
+	RECT _rc1;
+	RECT* _currentRC;
 
 	bool _stopCharacter;			//캐릭터가 멈췄니?(인벤토리 열때 사용)
+
+	vector<item*> _vItem;	//바닥에 있는 아이템과 몬스터가 떨구는 돈을 위한 아이템 벡터
+	vector<item*> _viItem;
+
+	bool _firstWave, _secondWave, _thirdWave;
 public:
 	stage3();
 	~stage3();
@@ -34,5 +40,17 @@ public:
 
 	virtual void setInventory(inventory* inven) { _inven = inven; }
 	virtual inventory* getInventory(){ return _inven; }
+
+	void dropMoney(POINT point, int won);	//돈 드랍 함수 몬스터가 죽었을경우 이것 호출하면 됩니다.
+
+	void makeEnemy();	//몬스터 생성 함수
+
+	void characterMovement();							//캐릭터 키매지저를 관리하는 함수
+
+	void addImage();		//이미기 추가해주는 함수 이후 이미지는 여기서 add하는걸로
+	void initialization();	//변수들 new선언 및 init 해주는 함수 이후 new 및 init은 여기서 하는걸로
+	void singletonInit();	//init에서 싱글톤들 세팅해주는 함수 이후 세팅은 여기서 하는걸로
+	void draw();			//그려주는 함수 이후 렌더는 여기서 하는걸로
+
 };
 
