2f3bd9d0161dbc9ae66e28e5be6dbf1130f43623
preun <woghtjdah@naver.com>
2018-01-03 17:02:43 +0900

    .

--- teamProject/mainGame.cpp
+++ teamProject/mainGame.cpp
@@ -13,27 +13,7 @@ HRESULT mainGame::init()			//초기화 함수
 {
 	gameNode::init(true);
 
-	SOUNDMANAGER->addSound("게임시작화면", "./sounds/게임시작화면.mp3", true, false);
-	SOUNDMANAGER->addSound("스테이지1", "./sounds/스테이지1.mp3", true, true);
-	SOUNDMANAGER->addSound("스테이지2", "./sounds/스테이지2.mp3", true, true);
-	SOUNDMANAGER->addSound("스테이지3", "./sounds/스테이지3.mp3", true, true);
-	SOUNDMANAGER->addSound("크억(죽을때)", "./sounds/크억(죽을때).wav", false, false);
-	SOUNDMANAGER->addSound("흐앗(발차기할때)", "./sounds/흐앗(발차기할때).wav", false, false);
-	SOUNDMANAGER->addSound("흣(때릴때)", "./sounds/흣(때릴때).wav", false, false);
-	SOUNDMANAGER->addSound("펀치맞을때", "./sounds/펀치맞을때.wav", false, false);
-
-	//병철 추가 // 스테이지 1 씬 //20171227 7:43
-	SCENEMANAGER->addScene("스테이지00", new stage1);
-	SCENEMANAGER->addScene("스테이지01", new stage2);
-
-	SCENEMANAGER->addScene("스테이지02", new stage3);
-
-	//재호 : 게임시작화면
-	SCENEMANAGER->addScene("스타트", new startscene);
-
-	//재호 : 1,3스테이지 대화화면
-	SCENEMANAGER->addScene("1스테이지대화", new stage1Story);
-	SCENEMANAGER->addScene("3스테이지대화", new stage3Story);
+	addImage();
 
 	//SCENEMANAGER->changeScene("스테이지00");
 
@@ -41,7 +21,7 @@ HRESULT mainGame::init()			//초기화 함수
 	SCENEMANAGER->changeScene("스타트", false);
 	//SCENEMANAGER->changeScene("스테이지02", false);
 
-	IMAGEMANAGER->addImage("유아이박스", "./images/ui_box.bmp", 1152, 144, true, RGB(255, 0, 255));
+	
 
 
 	return S_OK;
@@ -94,7 +74,12 @@ void mainGame::render()		//그려주는 함수(a.k.a WM_PAINT)
 
 		SCENEMANAGER->render();
 		IMAGEMANAGER->findImage("유아이박스")->render(getMemDC(), 0, WINSIZEY - IMAGEMANAGER->findImage("유아이박스")->getHeight());
-
+		IMAGEMANAGER->findImage("피박스")->render(getMemDC(), 400, 400);
+		/*for (int i = 0;
+			SCENEMANAGER->getCurrentScene()->getMainPlayer()->getHP(); i++)
+		{
+			IMAGEMANAGER->findImage("피박스")->render(getMemDC(), 400, 400);
+		}*/
 	}
 
 	//재호 : 이미지 출력 실험용
@@ -105,5 +90,43 @@ void mainGame::render()		//그려주는 함수(a.k.a WM_PAINT)
 	//==================== 건들지마라 =======================
 	TIMEMANAGER->render(getMemDC());
 	this->getBackBuffer()->render(getHDC(), 0, 0);
+
+}
+void mainGame::addImage()		//이미기 추가해주는 함수 이후 이미지는 여기서 add하는걸로
+{
+	SOUNDMANAGER->addSound("게임시작화면", "./sounds/게임시작화면.mp3", true, false);
+	SOUNDMANAGER->addSound("스테이지1", "./sounds/스테이지1.mp3", true, true);
+	SOUNDMANAGER->addSound("스테이지2", "./sounds/스테이지2.mp3", true, true);
+	SOUNDMANAGER->addSound("스테이지3", "./sounds/스테이지3.mp3", true, true);
+	SOUNDMANAGER->addSound("크억(죽을때)", "./sounds/크억(죽을때).wav", false, false);
+	SOUNDMANAGER->addSound("흐앗(발차기할때)", "./sounds/흐앗(발차기할때).wav", false, false);
+	SOUNDMANAGER->addSound("흣(때릴때)", "./sounds/흣(때릴때).wav", false, false);
+	SOUNDMANAGER->addSound("펀치맞을때", "./sounds/펀치맞을때.wav", false, false);
+
+	//병철 추가 // 스테이지 1 씬 //20171227 7:43
+	SCENEMANAGER->addScene("스테이지00", new stage1);
+	SCENEMANAGER->addScene("스테이지01", new stage2);
+
+	SCENEMANAGER->addScene("스테이지02", new stage3);
+
+	//재호 : 게임시작화면
+	SCENEMANAGER->addScene("스타트", new startscene);
+
+	//재호 : 1,3스테이지 대화화면
+	SCENEMANAGER->addScene("1스테이지대화", new stage1Story);
+	SCENEMANAGER->addScene("3스테이지대화", new stage3Story);
+
+	IMAGEMANAGER->addImage("유아이박스", "./images/ui_box.bmp", 1152, 144, true, RGB(255, 0, 255));
+
+
+	IMAGEMANAGER->addImage("피박스", "./images/hprect.bmp", 24, 24, true, RGB(255, 0, 255));
+
+}
+void mainGame::initialization()	//변수들 new선언 및 init 해주는 함수 이후 new 및 init은 여기서 하는걸로
+{
+
+}
+void mainGame::singletonInit()	//init에서 싱글톤들 세팅해주는 함수 이후 세팅은 여기서 하는걸로		  
+{
 
 }
--- teamProject/mainGame.h
+++ teamProject/mainGame.h
@@ -50,7 +50,9 @@ public:
 	virtual void update();			
 	virtual void render();	
 
-
+	void addImage();		//이미기 추가해주는 함수 이후 이미지는 여기서 add하는걸로
+	void initialization();	//변수들 new선언 및 init 해주는 함수 이후 new 및 init은 여기서 하는걸로
+	void singletonInit();	//init에서 싱글톤들 세팅해주는 함수 이후 세팅은 여기서 하는걸로
 
 
 	mainGame();
